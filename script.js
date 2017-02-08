67d67d7a77ff7e3fee8e6cee5c979bfc6f2cb0ed
Mitchell Blom <mitchellblom@gmail.com>
2017-02-07 20:40:55 -0600

    reset button now resets everything

--- script.js
+++ script.js
@@ -6,9 +6,15 @@ var newTemp = document.getElementById("newTemp");
 var result;
 var tempColor;
 
+var resetButton = document.getElementById("reset");
+
 // Assign a function to be executed when the button is clicked
 // aka "when i 'click' this button, run that funciton"
 button.addEventListener("click", determineConverter);
+
+resetButton.addEventListener("click", function() {
+	newTemp.className = "";
+});
 
 // This function should determine which conversion should happen based on which radio button is selected.
 
@@ -16,8 +22,8 @@ function determineConverter (clickEvent) {
 // declaring a variable and assigning only the radio buttons with the name "units"
   var units = document.getElementsByName("units")
 // checking if a radio named "units" is checked
-  	units.forEach(function(x) {
-  	if (x.checked) {
+  	units.forEach(function(radioButton) {
+  	if (radioButton.checked) {
 // if a radio named "units" is checked, this takes value from "converter" field to run this switch
   		var input = document.getElementById("converter").value;
   		var runSwitch = true;
@@ -26,10 +32,9 @@ function determineConverter (clickEvent) {
   			alert("Please input a number");
   			runSwitch = false;
   		}	
-
   		if (runSwitch) {
 // switch does one thing if "cel" radio is checked and another if "fah" is checked
-	  		switch(x.id) {
+	  		switch(radioButton.id) {
 	  			case "cel": 
 					result = input * (9/5) + 32;
 					tempColor = "cel";
@@ -39,13 +44,11 @@ function determineConverter (clickEvent) {
 					tempColor = "fah";
 				break;
 // if no unit is selected, user is alerted
-				default: 
-					alert("You didn't select a unit!");
-				break;																	// try this without the break after styling
+				default:
+				break;
 	  		}
 // prints new temp to "result" field
 	  		newTemp.value = result;
-
 	  		if (tempColor = "cel") {
 	  			if (result > 32) {
 	  				newTemp.className = "red";
@@ -63,9 +66,7 @@ function determineConverter (clickEvent) {
 	  				newTemp.className = "blue";
 		  		}
 	  		}
-
 	  	}
-
   	}
   }); // <--- placement of this semicolon alluded me until a second set of eyes helped place it properly!
 
